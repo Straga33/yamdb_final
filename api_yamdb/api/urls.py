862db4e06538d656e9587080dee9ec4a748d238e
Michail Baskov <baem-festa@yandex.ru>
2022-09-09 23:43:08 +0300

    correct_workflow_test

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,7 +7,7 @@ from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
 
 app_name = 'api'
 
-router = DefaultRouter()
+router = DefaultRouter() #comment
 
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
